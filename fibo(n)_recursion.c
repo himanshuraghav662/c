5a518092d26162f9152b540c4d786f48967a918a
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2023-01-03 10:49:48 +0530

    Create fibo(n)_recursion.c

---
+++ fibo(n)_recursion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int fib(int n)
+{
+  
+}
+void main()
+{
+int n,fibo;
+printf("enter a no:");
+scanf("%d",&n);
+fibo=fib(n);
+printf("nth term =%d",fibo);
+getch();
+}
